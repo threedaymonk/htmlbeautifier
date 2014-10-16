b7e7af410752e1ffb94800826a331715adf10b71
Jakub Jirutka <jakub@jirutka.cz>
2014-10-16 02:12:10 +0200

    Improve Beautifier's interface
    Writing directly to standard output is less useful than having a
    sensible interface.
    
    Before:
    
        output = ''
        HtmlBeautifier::Beautifier.new(output).scan(html)
    
    After:
    
        output = HtmlBeautifier.beautify(html)

--- lib/htmlbeautifier.rb
+++ lib/htmlbeautifier.rb
@@ -1,2 +1,14 @@
-require 'htmlbeautifier/beautifier'
+require 'htmlbeautifier/builder'
+require 'htmlbeautifier/html_parser'
 require 'htmlbeautifier/version'
+
+module HtmlBeautifier
+
+  # Returns a beautified HTML/HTML+ERB document as a String.
+  # html must be an object that responds to +#to_s+.
+  def self.beautify(html, tab_stops: 2)
+    ''.tap do |output|
+      HtmlParser.new.scan html.to_s, Builder.new(output, tab_stops)
+    end
+  end
+end
