45ad8cd6aa39e68a1618c16a15308ada7b7cdf41
Jakub Jirutka <jakub@jirutka.cz>
2014-12-28 17:05:04 +0100

    Add newlines after block-like elements

--- lib/htmlbeautifier/html_parser.rb
+++ lib/htmlbeautifier/html_parser.rb
@@ -7,6 +7,12 @@ module HtmlBeautifier
       area | base | br | col | command | embed | hr | img | input | keygen |
       link | meta | param | source | track | wbr
     )}mix
+    HTML_BLOCK_ELEMENTS = %r{(?:
+      address | blockquote | center | dd | dir | div | dl | dt | fieldset |
+      form | h1 | h2 | h3 | h4 | h5 | h6 | hr | isindex | li | menu |
+      noframes | noscript | ol | p | pre | table | tbody | td | tfoot | th |
+      thead | tr | ul
+    )}mix
 
     def initialize
       super do |p|
@@ -20,20 +26,24 @@ module HtmlBeautifier
           :standalone_element
         p.map %r{<!.*?>}m,
           :standalone_element
-        p.map %r{(<script#{ELEMENT_CONTENT}>)(.*?)(</script>)}m,
+        p.map %r{(<script#{ELEMENT_CONTENT}>)(.*?)(</script>)}mi,
           :foreign_block
-        p.map %r{(<style#{ELEMENT_CONTENT}>)(.*?)(</style>)}m,
+        p.map %r{(<style#{ELEMENT_CONTENT}>)(.*?)(</style>)}mi,
           :foreign_block
         p.map %r{<#{ELEMENT_CONTENT}/>}m,
           :standalone_element
         p.map %r{<#{HTML_VOID_ELEMENTS}(?: #{ELEMENT_CONTENT})?>}m,
           :standalone_element
+        p.map %r{</#{HTML_BLOCK_ELEMENTS}>}m,
+          :close_block_element
+        p.map %r{<#{HTML_BLOCK_ELEMENTS}(?: #{ELEMENT_CONTENT})?>}m,
+          :open_block_element
         p.map %r{</#{ELEMENT_CONTENT}>}m,
           :close_element
         p.map %r{<#{ELEMENT_CONTENT}>}m,
           :open_element
-        p.map %r{\s+},
-          :whitespace
+        p.map %r{\s*\r?\n\s*}m,
+          :new_line
         p.map %r{[^<]+},
           :text
       end
