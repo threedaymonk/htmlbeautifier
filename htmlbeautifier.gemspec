902cdb3553bd766e5e4901a583064e1b414e68ed
Paul Battley <pbattley@gmail.com>
2014-12-30 21:26:55 +0000

    Support back to Ruby 1.9.2

--- htmlbeautifier.gemspec
+++ htmlbeautifier.gemspec
@@ -17,7 +17,7 @@ spec = Gem::Specification.new do |s|
 
   s.require_paths     = ["lib"]
 
-  s.required_ruby_version = '>= 2.0'
+  s.required_ruby_version = '>= 1.9.2'
 
   s.add_development_dependency "rake", "~> 0"
 end
--- lib/htmlbeautifier.rb
+++ lib/htmlbeautifier.rb
@@ -6,9 +6,10 @@ module HtmlBeautifier
 
   # Returns a beautified HTML/HTML+ERB document as a String.
   # html must be an object that responds to +#to_s+.
-  def self.beautify(html, tab_stops: 2)
-    ''.tap do |output|
+  def self.beautify(html, options = {})
+    tab_stops = options.fetch(:tab_stops, 2)
+    ''.tap { |output|
       HtmlParser.new.scan html.to_s, Builder.new(output, tab_stops)
-    end
+    }
   end
 end
