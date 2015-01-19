376577f8073f601271deffb1594724404071ff7b
Paul Battley <pbattley@gmail.com>
2015-01-19 15:38:25 +0300

    Remove duplication in executable spec

--- spec/executable_spec.rb
+++ spec/executable_spec.rb
@@ -2,6 +2,20 @@ require 'shellwords'
 require 'fileutils'
 
 describe 'bin/htmlbeautifier' do
+  before do
+    FileUtils.mkdir_p path_to('tmp')
+  end
+
+  def write(path, content)
+    File.open(path, 'w') do |f|
+      f.write content
+    end
+  end
+
+  def read(path)
+    File.read(path)
+  end
+
   def path_to(*partial)
     File.join(File.expand_path('../..', __FILE__), *partial)
   end
@@ -18,48 +32,36 @@ describe 'bin/htmlbeautifier' do
   end
 
   it 'beautifies a file in place' do
-    FileUtils.mkdir_p path_to('tmp')
     input = "<p>\nfoo\n</p>"
     expected = "<p>\n  foo\n</p>\n"
     path = path_to('tmp', 'in-place.html')
-    File.open(path, 'w') do |f|
-      f.write input
-    end
+    write path, input
 
     system '%s %s' % [command, escape(path)]
 
-    output = File.read(path)
-    expect(output).to eq(expected)
+    expect(read(path)).to eq(expected)
   end
 
   it 'beautifies a file from stdin to stdout' do
-    FileUtils.mkdir_p path_to('tmp')
     input = "<p>\nfoo\n</p>"
     expected = "<p>\n  foo\n</p>\n"
     in_path = path_to('tmp', 'input.html')
     out_path = path_to('tmp', 'output.html')
-    File.open(in_path, 'w') do |f|
-      f.write input
-    end
+    write in_path, input
 
     system '%s < %s > %s' % [command, escape(in_path), escape(out_path)]
 
-    output = File.read(out_path)
-    expect(output).to eq(expected)
+    expect(read(out_path)).to eq(expected)
   end
 
   it 'allows a configurable number of tab stops' do
-    FileUtils.mkdir_p path_to('tmp')
     input = "<p>\nfoo\n</p>"
     expected = "<p>\n   foo\n</p>\n"
     path = path_to('tmp', 'in-place.html')
-    File.open(path, 'w') do |f|
-      f.write input
-    end
+    write path, input
 
     system '%s --tab-stops=3 %s' % [command, escape(path)]
 
-    output = File.read(path)
-    expect(output).to eq(expected)
+    expect(read(path)).to eq(expected)
   end
 end
