fd6e5cf9d9b8a03f5a7d58c4e55ffb2dc9101436
Paul Battley <pbattley@gmail.com>
2021-11-26 00:13:15 +0000

    Release 1.4.0

--- lib/htmlbeautifier/version.rb
+++ lib/htmlbeautifier/version.rb
@@ -3,8 +3,8 @@
 module HtmlBeautifier # :nodoc:
   module VERSION # :nodoc:
     MAJOR = 1
-    MINOR = 3
-    TINY  = 1
+    MINOR = 4
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join(".")
   end
